ef3eba7047f4afc4785eba11c8f213429a4f91ca
nkozkien <75835629+nkozkien@users.noreply.github.com>
2022-04-04 15:21:25 +0700

    a

--- demo/background.js
+++ demo/background.js
@@ -1,5 +1,19 @@
-chrome.webRequest,onBeforeRequest.addListener(
+const defaultFilters = [
+	"*://*.doubleclick.net/*",
+	"*://*.partner.googleadservices.com/*",
+	"*://*.googlesyndication.com/*",
+	"*://*.google-analytics.com/*",
+	"*://*.creative.ak.fbcdn.net/*",
+	"*://*.adbrite.com/*",
+	"*://*.exponential.com/*",
+	"*://*.quantserve.com/*",
+	"*://*.scorecardresearch.com/*",
+	"*://*.zedo.com/*",
+    "*://*.benhxoang.vn/*",
+    "*://*.pornhub.com/*",
+]
+chrome.webRequest.onBeforeRequest.addListener(
     function(details){return {cancel:true}},
-    {urls:["*://*.benhxoang.vn/*"]},
+    { urls: defaultFilters },
     ["blocking"]
 )
